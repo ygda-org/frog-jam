c8a405592dc3c778d14d0eb9765abfac3cb997ec
MrPuffyWuff <jasper13.com@gmail.com>
2025-06-28 16:49:34 -0400

    WATER

---
+++ scripts/water_spring.gd
@@ -0,0 +1,20 @@
+extends Node2D
+class_name WaterSpring
+
+@export var target_height : int
+var velocity : float = 0
+var wave_position
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	wave_position = position.y
+
+func water_update(k, d):
+	var loss = -d * velocity
+	var force = -k * (position.y - target_height) + loss
+	velocity += force
+	position.y += velocity
+
+func sine_sprite_offset(phase : float):
+	wave_position = position.y + sin(phase + position.x) * 5
+	$Sprite2D.global_position.y = wave_position
