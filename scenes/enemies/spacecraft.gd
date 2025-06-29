1acfd6465390a258b8829ce7cbfad3e3e323ae7c
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-06-29 10:43:04 -0400

    Spacecraft movement! Doesn't collide w/ player & do dmg tho yet

--- scenes/enemies/spacecraft.gd
+++ scenes/enemies/spacecraft.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
+const SPEED = 200
+
 var random = RandomNumberGenerator.new()
+
+var starting_side
+var rotationdeg
 
 var behavior # does it act like sputnik or shuttle, 0 for sputnik 1 for shuttle
 # Called when the node enters the scene tree for the first time.
@@ -14,8 +19,15 @@ func _ready():
 		$Sprite2D.texture = load("res://assets/images/Spacecraft 2.PNG")
 		scale = Vector2(0.5, 0.5)
 		$Collision1.disabled = false
-
+		
+	starting_side = get_viewport().size.x if random.randi_range(0,1) == 0 else 0
+	position.x = starting_side
+	rotationdeg = random.randi_range(60, 120)
+	rotation = (rotationdeg * PI) / 180
+	if starting_side != 0:
+		rotation += PI
+	velocity = Vector2(0, -SPEED).rotated(rotation)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	move_and_slide()
