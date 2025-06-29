dd12b2500391562459c8e9f57accceb5d7799340
Dev Makwana <legend.destroyer.d@gmail.com>
2025-06-29 17:30:27 -0400

    Fixed bird motion

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -1,24 +1,19 @@
 extends CharacterBody2D
 
-var VIEWPORT_WIDTH = 0
-
 const SPEED = 200
 
 var random = RandomNumberGenerator.new()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	VIEWPORT_WIDTH = get_viewport().size
 	if random.randi_range(0, 1) == 1:
 		velocity = Vector2(SPEED ,0)
 		scale.x *= -1
 	else:
 		velocity = Vector2(-SPEED, 0)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if position.x > VIEWPORT_WIDTH.x or position.x < -VIEWPORT_WIDTH.x:
-		scale.x *= -1
-		velocity.x *= -1
 	move_and_slide()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	scale.x *= -1
+	velocity.x *= -1
