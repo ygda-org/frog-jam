943a03ecafb669ce989b453ff0a8d8be67b556a1
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-06-29 13:29:52 -0400

    frog bubble random flip

--- scripts/frog_bubble.gd
+++ scripts/frog_bubble.gd
@@ -3,6 +3,12 @@ extends Area2D
 
 @export var frog_data: FrogData
 
+var random = RandomNumberGenerator.new()
+
+func _ready():
+	if random.randi_range(0, 1) == 1:
+		scale.x *= -1
+
 func set_data(new_data: FrogData) -> void:
 	self.frog_data = new_data
 	$FrogSprite.texture = new_data.texture
