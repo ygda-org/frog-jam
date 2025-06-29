cf7012903f2c10d9ac43e21dbd14fcd60a13617a
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-06-29 11:26:32 -0400

    Got rid of print statement

--- scripts/fly.gd
+++ scripts/fly.gd
@@ -3,6 +3,5 @@ extends StaticBody2D
 var random = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(random.randi_range(0, 1))
 	if random.randi_range(0, 1) == 1:
 		$Anim.flip_h = true
