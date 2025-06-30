54f63d2c0c28eaa98360fb5be553e6cfadccd72c
Dev Makwana <legend.destroyer.d@gmail.com>
2025-06-29 20:39:59 -0400

    Reexported and fixed water size

--- scripts/water_body.gd
+++ scripts/water_body.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 	position.x -= VIEWPORT_SIZE.x/2
 	
 	var window_width = VIEWPORT_SIZE.x
-	for x in range (-2300, 7100 + SPRING_GAP_DIST, SPRING_GAP_DIST):
+	for x in range (-3000, 7100 + SPRING_GAP_DIST, SPRING_GAP_DIST):
 		var instance = WATER_SPRING.instantiate()
 		instance.target_height = starting_y
 		instance.position.y = starting_y
@@ -47,7 +47,7 @@ func update_all_water(passes : int):
 
 func draw_water_polygon():
 	var points : PackedVector2Array
-	points.append(Vector2(-2300,3000))
+	points.append(Vector2(-3000,3000))
 	for spring in springs:
 		points.append(Vector2(spring.position.x, spring.wave_position))
 	points.append(Vector2(7100, 3000))
