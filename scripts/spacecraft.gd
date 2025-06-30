6ebd4d6a85033de2659086695cb6191226d73cad
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2025-06-29 20:36:59 -0400

    Spacecraft size & kite dmg

--- scripts/spacecraft.gd
+++ scripts/spacecraft.gd
@@ -16,11 +16,11 @@ func _ready():
 	behavior = random.randi_range(0, 1)
 	if behavior == 0:
 		$Sprite2D.texture = load("res://assets/images/Spacecraft 1.PNG")
-		scale = Vector2(0.2, 0.2)
+		scale = Vector2(0.5, 0.5)
 		$Collision2.disabled = false
 	elif behavior == 1:
 		$Sprite2D.texture = load("res://assets/images/Spacecraft 2.PNG")
-		scale = Vector2(0.5, 0.5)
+		scale = Vector2(1.25, 1.25)
 		$Collision1.disabled = false
 		
 	starting_side = get_viewport().size.x if random.randi_range(0,1) == 0 else 0
@@ -36,7 +36,7 @@ func _physics_process(delta):
 	move_and_slide()
 	for i in range(get_slide_collision_count()):
 		if get_slide_collision(i).get_collider().name == "Player":
-			get_slide_collision(i).get_collider().hit(10)
+			get_slide_collision(i).get_collider().hit(15)
 			
 			velocity = velocity.length() * Vector2.RIGHT.rotated(get_slide_collision(i).get_angle())
 			get_slide_collision(i).get_collider().velocity = get_slide_collision(i).get_collider().velocity.length() * Vector2.LEFT.rotated(get_slide_collision(i).get_angle()) * 0.5
