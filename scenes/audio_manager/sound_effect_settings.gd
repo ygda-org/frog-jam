77ed0caec0f61a3c88011e81a058e62b29c4511a
MrPuffyWuff <jasper13.com@gmail.com>
2025-06-29 20:11:59 -0400

    asdfghj

--- scenes/audio_manager/sound_effect_settings.gd
+++ scenes/audio_manager/sound_effect_settings.gd
@@ -25,3 +25,4 @@ enum SOUND_EFFECT_LABEL{
 @export var stream : AudioStream
 @export_range(-40,24) var volume : float = 1.0
 @export_range(0.01, 4.0) var pitch : float = 1.0
+@export var audio_start_offset : float = 0.0
